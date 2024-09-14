19637317581c6ce67a3e3e1d111cffc4fde977b2
user <user@DESKTOP-VTNNO66.mshome.net>
2024-09-14 10:28:17 +0530

    Added new pompage-Products

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectRepositoryUtility/Products.java
@@ -0,0 +1,15 @@
+package com.comcast.crm.objectRepositoryUtility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+	@FindBy(xpath="//img[@alt='Create Product...']")
+	private WebElement pdaddbtn;
+	
+	public WebElement getPdAddBtn() {
+		return pdaddbtn;
+		
+	}
+
+}
